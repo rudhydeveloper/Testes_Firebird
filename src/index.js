7f2a322e14ad19be34c8bb833ccc6d26d344f3a0
RudhyDev <rudhy.pereira@gmail.com>
2023-02-06 21:26:29 -0300

    refactor: :recycle: mais inteligente

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/produtos', (req, res) => {
-  executeQuery('SELECT * FROM PRODUTO', [], (err, result) => {
+  let filtro = []
+  let ssql = 'SELECT * FROM TAB_PRODUTO'
+
+  if (req.query.descricao) {
+    filtro.push(req.query.descricao)
+    ssql += ' AND descricao LIKE ?'
+  }
+
+  executeQuery(ssql, filtro, (err, result) => {
     if (err) return res.status(400).json({ error: err.message })
     return res.json(result)
   })
