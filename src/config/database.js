87bbfb5c629201b958a33123081f9db79ef091bf
RudhyDev <rudhy.pereira@gmail.com>
2023-02-06 21:15:29 -0300

    refactor: :recycle: refatorando

--- src/config/database.js
+++ src/config/database.js
@@ -11,4 +11,17 @@ const dbOptions = {
   pageSize: 4096 // default when creating database
 }
 
-export default dbOptions
+const executeQuery = (ssql, params, callback) => {
+  firebird.attach(dbOptions, function (err, db) {
+    if (err) return callback(err, [])
+    // db = DATABASE
+    db.query(ssql, params, function (err, result) {
+      // IMPORTANT: close the connection
+      db.detach()
+      if (err) return callback(err, [])
+      else return callback(null, result)
+    })
+  })
+}
+
+export { executeQuery }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 
-import firebird from 'node-firebird'
-import dbOptions from './config/database.js'
+import { executeQuery } from './config/database.js'
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 
 app.get('/produtos', (req, res) => {
-  firebird.attach(dbOptions, function (err, db) {
-    if (err) return res.status(500).json({ error: err.message })
-
-    // db = DATABASE
-    db.query('SELECT * FROM TABLE nome_da_tabela', function (err, result) {
-      // IMPORTANT: close the connection
-      db.detach()
-
-      if (err) return res.status(500).json({ error: err.message })
-      else return res.status(200).json(result)
-    })
+  executeQuery('SELECT * FROM PRODUTO', [], (err, result) => {
+    if (err) return res.status(400).json({ error: err.message })
+    return res.json(result)
   })
 })
 
