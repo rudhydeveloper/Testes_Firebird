6398f24c01cbfa199c2f74d8a40a6b90ed4d4e4e
RudhyDev <rudhy.pereira@gmail.com>
2023-02-06 22:23:55 -0300

    rota post

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+//--------------------------
+
 app.get('/produtos', (req, res) => {
   let filtro = []
   let ssql = 'SELECT * FROM TAB_PRODUTO WHERE 1=1'
@@ -26,6 +28,20 @@ app.get('/produtos', (req, res) => {
     return res.json(result)
   })
 })
+
+//--------------------------
+
+app.post('/produtos', (req, res) => {
+  let ssql =
+    'INSERT INTO TAB_PRODUTO (descricao, valor) VALUES (?, ?) RETURNING  ID_PRODUTO'
+
+  executeQuery(ssql, [req.body.descricao, req.body.valor], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message })
+    return res.json({ id_produto: result[0].ID_PRODUTO })
+  })
+})
+
+//--------------------------
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
